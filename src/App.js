ac21f2bebb31d619c51ab536603c9794014b8f88
GoranAviani <goranaviani@gmail.com>
2022-05-03 18:12:52 +0200

    javascript_array_cardio_practice_day_1

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
     // 1. Filter the list of inventors for those who were born in the 1500's
     const result1 = inventors.filter(inventor => inventor.year > 1500 && inventor.year < 1600)
     console.table(result1)
-    const displayResult1 = result1.map(res => <p>{res.first} {res.last}</p>)
+    const displayResult1 = result1.map(res => <p>{res.first} {res.last} {res.year} {res.passed}</p>)
 
     // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
