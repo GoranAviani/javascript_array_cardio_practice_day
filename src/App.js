3f526104aa99462042003bcc1f129cd7ad21a823
GoranAviani <goranaviani@gmail.com>
2022-05-03 18:03:03 +0200

    javascript_array_cardio_practice_day_1

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ function App() {
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
-
+        const result1 = inventors.filter(inventor => inventor.year > 1400 && inventor.year <1500)
+        console.log(result1)
 
   return (
    <div>
