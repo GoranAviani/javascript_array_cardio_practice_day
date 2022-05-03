0db8d8395b78be39e8ba57fc8d9b647bc848c0d6
GoranAviani <goranaviani@gmail.com>
2022-05-03 18:07:23 +0200

    javascript_array_cardio_practice_day_1

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 const inventors = [
-      { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-      { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-      { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-      { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-      { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-      { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-      { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-      { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-      { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-      { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-      { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-      { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-    ];
+    {first: 'Albert', last: 'Einstein', year: 1879, passed: 1955},
+    {first: 'Isaac', last: 'Newton', year: 1643, passed: 1727},
+    {first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642},
+    {first: 'Marie', last: 'Curie', year: 1867, passed: 1934},
+    {first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630},
+    {first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543},
+    {first: 'Max', last: 'Planck', year: 1858, passed: 1947},
+    {first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979},
+    {first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852},
+    {first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905},
+    {first: 'Lise', last: 'Meitner', year: 1878, passed: 1968},
+    {first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909}
+];
 
 
 function App() {
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
-        const result1 = inventors.filter(inventor => inventor.year > 1500 && inventor.year <1600)
-        console.table(result1)
+    const result1 = inventors.filter(inventor => inventor.year > 1500 && inventor.year < 1600)
+    console.table(result1)
 
-        // Array.prototype.map()
+    // Array.prototype.map()
     // 2. Give us an array of the inventors' first and last names
     const result2 = inventors.map(inventor => `${inventor.first} ${inventor.last}`)
     console.table(result2)
 
-        // Array.prototype.sort()
+    // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
 
 
-
-  return (
-   <div>
-   APP
-    </div>
-  );
+    return (
+        <div>
+            APP
+        </div>
+    );
 }
 
 export default App;
