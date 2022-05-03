7790e03713f3701e37c5ba8282a4f17cc3e4b5bd
GoranAviani <goranaviani@gmail.com>
2022-05-03 18:10:09 +0200

    javascript_array_cardio_practice_day_1

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ const inventors = [
 ];
     const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
+   const morePeople = [
+      { name: 'Dante', year: 1988 },
+      { name: 'Randall', year: 1987 },
+      { name: 'Bob', year: 1944 },
+      { name: 'Jay', year: 2016}
+    ];
+
+
 
 function App() {
 
